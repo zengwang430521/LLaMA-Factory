935dd596961e82042973ed0b2f283301c7f824cb
zengwang <zengwang@tetras.ai>
2025-02-06 14:47:36 +0800

    debug

--- src/llamafactory/data/template.py
+++ src/llamafactory/data/template.py
@@ -1174,6 +1174,24 @@ _register_template(
 
 
 _register_template(
+    name="qwen2_vl_stream_v2",
+    format_user=StringFormatter(slots=["<|im_start|>user\n{{content}}<|im_end|>\n"]),
+    format_system=StringFormatter(slots=["<|im_start|>system\n{{content}}<|im_end|>\n"]),
+    format_assistant=StringFormatter(slots=["<|im_start|>assistant\n", "{{content}}", {"eos_token"}]),
+    format_function=FunctionFormatter(slots=["{{content}}", "<|im_end|>"], tool_format="qwen"),
+    format_observation=StringFormatter(
+        slots=["<|im_start|>user\n<tool_response>\n{{content}}\n</tool_response><|im_end|>\n<|im_start|>assistant\n"]
+    ),
+    format_tools=ToolFormatter(tool_format="qwen"),
+    format_separator=EmptyFormatter(slots=["\n"]),
+    default_system="You are a helpful assistant.",
+    stop_words=["<|im_end|>"],
+    mm_plugin=get_mm_plugin(name="qwen2_vl_stream", image_token="<|image_pad|>", video_token="<|video_pad|>"),
+)
+
+
+
+_register_template(
     name="sailor",
     format_user=StringFormatter(slots=["<|im_start|>question\n{{content}}<|im_end|>\n<|im_start|>answer\n"]),
     format_system=StringFormatter(slots=["<|im_start|>system\n{{content}}<|im_end|>\n"]),
