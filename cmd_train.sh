82a503d4d9ec899cfa1106c286163ad41d785bd7
zengwang <zengwang@tetras.ai>
2025-02-07 16:28:10 +0800

    debug

---
+++ cmd_train.sh
@@ -0,0 +1,56 @@
+cd /afs/zengwang/projects/task_define_service/LLaMA-Factory
+
+NPROC_PER_NODE=8
+NNODES=1
+NODE_RANK=0
+MASTER_ADDR="localhost"
+MASTER_PORT=29500
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $NPROC_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+    "
+echo "DISTRIBUTED_ARGS:$DISTRIBUTED_ARGS"
+
+torchrun $DISTRIBUTED_ARGS src/train.py \
+--deepspeed examples/deepspeed/ds_z3_config.json \
+--stage sft \
+--do_train \
+--warmup_steps 100 \
+--weight_decay 0.1 \
+--learning_rate 1.0e-5 \
+--lr_scheduler_type cosine \
+--overwrite_cache \
+--overwrite_output_dir \
+--logging_steps 10 \
+--ddp_timeout 9000  \
+--bf16 \
+--per_device_train_batch_size 1 \
+--gradient_accumulation_steps 16 \
+--model_name_or_path /afs/zengwang/ckpt/Stream-Qwen2-VL-7B-Instruct \
+--template qwen2_vl_stream_v2 \
+--freeze_vision_tower \
+--finetuning_type lora \
+--lora_rank 8 \
+--additional_target stream_head \
+--video_resolution 65536 \
+--video_fps 2 \
+--video_maxlen 64 \
+--cutoff_len 4096 \
+--num_train_epochs 1 \
+--save_steps 500 \
+--output_dir work_dirs/stream_v2_lora_1\
+--dataset \
+MMDuetIT_dvc_stream_v2_25k,\
+MMDuetIT_magqa_stream_v2_25k,\
+llava_video_0_30s_academic_mc_stream_v2_5k,\
+llava_video_0_30s_academic_oe_stream_v2_10k,\
+llava_video_0_30s_academic_cap_stream_v2_10k,\
+llava_video_0_30s_youtube_mc_stream_v2_5k,\
+llava_video_0_30s_youtube_oe_stream_v2_10k,\
+llava_video_0_30s_youtube_cap_stream_v2_10k
+
+#    --mask_history \
--- data_preprocess/llava_video.py
+++ data_preprocess/llava_video.py
@@ -1,5 +1,7 @@
 import json
 import os
+from os.path import join
+from tqdm import tqdm
 
 # root_dir = '/home/SENSETIME/zengwang/myprojects/task_define_service/data/LLaVA-Video-178K'
 # for root, dirs, files in os.walk(root_dir):
@@ -13,7 +15,49 @@ import os
 #             src_data = json.load(f)
 #         print(f"{src_file}: {len(src_data)}")
 
-src_file = '/home/SENSETIME/zengwang/myprojects/task_define_service/data/LLaVA-Video-178K/0_30_s_academic_v0_1/0_30_s_academic_oe_v0_1_qa_processed.json'
-with open(src_file, 'r') as f:
-    src_data = json.load(f)
-print(f"{src_file}: {len(src_data)}")
+
+tar_dir = '/home/SENSETIME/zengwang/myprojects/task_define_service/data/LLaVA-Video-178K/processed/v2'
+os.makedirs(tar_dir, exist_ok=True)
+
+for src_dir in [
+    "/home/SENSETIME/zengwang/myprojects/task_define_service/data/LLaVA-Video-178K/0_30_s_academic_v0_1",
+    "/home/SENSETIME/zengwang/myprojects/task_define_service/data/LLaVA-Video-178K/0_30_s_youtube_v0_1",
+    "/home/SENSETIME/zengwang/myprojects/task_define_service/data/LLaVA-Video-178K/1_2_m_academic_v0_1",
+    "/home/SENSETIME/zengwang/myprojects/task_define_service/data/LLaVA-Video-178K/1_2_m_youtube_v0_1"
+]:
+    for filename in os.listdir(src_dir):
+        if not filename.endswith('.json'):
+            continue
+
+        src_path = join(src_dir, filename)
+        tar_path = join(tar_dir, filename)
+
+        with open(src_path, 'r') as f:
+            src_data = json.load(f)
+
+        role_transfer_dict = {
+            "human": "user",
+            "gpt": "assistant"
+        }
+        tar_data = []
+        for src_item in tqdm(src_data):
+            t = 0
+            end_time = 100000
+            video_message = {"role": "user", "content": "<video>", "time": [0, end_time]}     # 完整视频
+            video_path = join('LLaVA-Video-178K', src_item['data_source'], src_item['video'])
+            messages, videos = [], []
+            messages.append(video_message)
+            videos.append(video_path)
+            for conv in src_item["conversations"]:
+                role = role_transfer_dict[conv["from"]]
+                content = conv["value"].replace('<image>\n', "")
+                messages.append({"role": role, "content": content, "time": [end_time, end_time]})
+            tar_item = {"messages": messages, "videos": videos}
+            tar_data.append(tar_item)
+        with open(tar_path, 'w', encoding='utf-8') as f:
+            json.dump(tar_data, f, ensure_ascii=False)
+        print(f"{filename}: {len(tar_data)}")
+
+
+
+
