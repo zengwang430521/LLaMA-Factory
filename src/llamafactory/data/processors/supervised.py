bcd8c74c60e4573661dfce6291fd75d50522fa66
zengwang <zengwang@tetras.ai>
2025-02-07 16:48:26 +0800

    debug

--- src/llamafactory/data/processors/supervised.py
+++ src/llamafactory/data/processors/supervised.py
@@ -215,8 +215,8 @@ def _encode_supervised_stream_example_v2(
     # <video> <text> <video> 交错的版本
     # stream labels 应该在每帧最后的 video token 和问题最后的文本token上计算
 
-    import pdb; pdb.set_trace()
-    print('Debug: 产生input_ids, labels, stream_labels')
+    # import pdb; pdb.set_trace()
+    # print('Debug: 产生input_ids, labels, stream_labels')
 
     # 判断视频应该怎么分段
     video_time_segs = []
