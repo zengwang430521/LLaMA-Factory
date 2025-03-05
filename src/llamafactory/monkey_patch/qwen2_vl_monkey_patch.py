f9e942c28f2375b07af7d77bd995f164df8868c9
zengwang <zengwang@tetras.ai>
2025-03-05 20:52:39 +0800

    debug

--- src/llamafactory/monkey_patch/qwen2_vl_monkey_patch.py
+++ src/llamafactory/monkey_patch/qwen2_vl_monkey_patch.py
@@ -126,8 +126,28 @@ class Qwen2VLStream(Qwen2VLForConditionalGeneration):
         "The image shows a street scene with a red stop sign in the foreground. In the background, there is a large red gate with Chinese characters ..."
         ```"""
 
-        import pdb; pdb.set_trace()
-        print('Debug: 模型forward')
+        # import pdb; pdb.set_trace()
+        # print('Debug: 模型forward')
+        # if not hasattr(self, "tokenizer"):
+        #     from transformers.models.auto import AutoTokenizer
+        #     self.tokenizer = AutoTokenizer.from_pretrained("/afs/zengwang/ckpt/Stream-Qwen2-VL-7B-Instruct")
+        #     input_texts = self.tokenizer.batch_decode(input_ids)
+        #     self.tokenizer.batch_decode(input_ids[labels!=-100])
+        #     indexs = (stream_labels == 0).nonzero()
+        #     for index in indexs:
+        #         idx_batch, idx_token = index;
+        #         print('-'* 50);
+        #         print(index);
+        #         idx_end = min(idx_token+20, input_ids.shape[1]-1);
+        #         idx_begin = max(idx_token-20, 0);
+        #         print(self.tokenizer.decode(input_ids[idx_batch, idx_begin:idx_token]) +
+        #               '\n@\n' + self.tokenizer.decode(input_ids[idx_batch, idx_token]) + '\n@\n' +
+        #               self.tokenizer.decode(input_ids[idx_batch, idx_token+1:idx_end]))
+        #
+        #         # print(self.tokenizer.decode(input_ids[idx_batch, idx_begin:idx_token]))
+        #         # print(self.tokenizer.decode(input_ids[idx_batch, idx_token:idx_end]))
+        #         (stream_labels == 0).nonzero()
+        #         self.tokenizer.batch_decode(input_ids[stream_labels!=-100])
 
         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
         output_hidden_states = (
