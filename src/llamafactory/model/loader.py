1c6d539b042dcf773bfb6e18359f1de64a02c8b5
zengwang <zengwang@tetras.ai>
2025-03-06 22:38:25 +0800

    debug

--- src/llamafactory/model/loader.py
+++ src/llamafactory/model/loader.py
@@ -129,8 +129,8 @@ def load_model(
     r"""
     Loads pretrained model.
     """
-    # import pdb; pdb.set_trace()
-    # print('Debug: 加载模型')
+    import pdb; pdb.set_trace()
+    print('Debug: 加载模型')
 
     init_kwargs = _get_init_kwargs(model_args)
     config = load_config(model_args)
