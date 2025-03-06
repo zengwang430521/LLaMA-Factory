e9be140ff63e63cd02bf10d79d15da6a577051b9
zengwang <zengwang@tetras.ai>
2025-03-06 22:17:55 +0800

    debug

--- src/llamafactory/data/mm_plugin.py
+++ src/llamafactory/data/mm_plugin.py
@@ -1405,7 +1405,9 @@ class Qwen2vlStreamPluginV3(Qwen2vlStreamPluginV2):
         videos: Sequence["VideoInput"],
         processor: Optional["ProcessorMixin"],
     ) -> List[Dict[str, str]]:
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
+        # print("DebugV3: process_messages")
+
         self._validate_input(images, videos)
         image_processor: "BaseImageProcessor" = getattr(processor, "image_processor")
         merge_length: int = getattr(image_processor, "merge_size") ** 2
