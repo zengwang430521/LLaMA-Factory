73fd0633910fa0390d010a633106abc002084d7c
zengwang <zengwang@tetras.ai>
2025-03-10 18:22:10 +0800

    debug

--- src/llamafactory/data/processors/supervised.py
+++ src/llamafactory/data/processors/supervised.py
@@ -608,8 +608,8 @@ def preprocess_supervised_dataset(
 
             num_image, num_video, num_video_grid = get_image_video_grid_num(messages[:reserved_message_num])
 
-            model_inputs["frame_idxs"].append(frame_idxs[:num_video_grid])
-            model_inputs["frame_times"].append(frame_times[:num_video_grid])
+            model_inputs["frame_idxs"].append(frame_idxs[:num_video])
+            model_inputs["frame_times"].append(frame_times[:num_video])
             model_inputs["video_grid_thw"].append(video_grid_thw[:num_video_grid])
 
             images = examples["_images"][i]
