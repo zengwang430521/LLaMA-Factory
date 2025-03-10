230a0d1f2b8d65ecbe06dbc98675591df22dcb43
zengwang <zengwang@tetras.ai>
2025-03-10 18:11:30 +0800

    debug

--- src/llamafactory/data/processors/supervised.py
+++ src/llamafactory/data/processors/supervised.py
@@ -602,6 +602,10 @@ def preprocess_supervised_dataset(
             model_inputs["stream_labels"].append(stream_labels)
 
             messages = examples["_prompt"][i] + examples["_response"][i]
+
+            if reserved_message_num < len(messages):
+                import pdb; pdb.set_trace()
+
             num_image, num_video, num_video_grid = get_image_video_grid_num(messages[:reserved_message_num])
 
             model_inputs["frame_idxs"].append(frame_idxs[:num_video_grid])
@@ -647,10 +651,6 @@ def preprocess_supervised_dataset(
             model_inputs["stream_labels"].append(stream_labels)
 
             messages = examples["_prompt"][i] + examples["_response"][i]
-            if reserved_message_num < len(messages):
-                import pdb; pdb.set_trace()
-
-
             num_image, num_video, num_video_grid = get_image_video_grid_num(messages[:reserved_message_num])
 
             model_inputs["frame_idxs"].append(frame_idxs[:num_video_grid])
