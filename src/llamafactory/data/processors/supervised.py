95e7be18a601af43568ec412ef364a99cbe57875
zengwang <zengwang@tetras.ai>
2025-03-10 15:20:30 +0800

    debug

--- src/llamafactory/data/processors/supervised.py
+++ src/llamafactory/data/processors/supervised.py
@@ -598,7 +598,6 @@ def preprocess_supervised_dataset(
             model_inputs["images"].append(examples["_images"][i])
             model_inputs["videos"].append(examples["_videos"][i])
 
-    import pdb; pdb.set_trace()
     return model_inputs
 
 
