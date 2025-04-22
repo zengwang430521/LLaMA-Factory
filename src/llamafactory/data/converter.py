a723bbb0d842649f60a9cad143d9383d40c5bd75
zengwang <zengwang@tetras.ai>
2025-04-22 21:46:16 +0800

    debug

--- src/llamafactory/data/converter.py
+++ src/llamafactory/data/converter.py
@@ -212,9 +212,129 @@ class SharegptDatasetConverter(DatasetConverter):
         return output
 
 
+@dataclass
+class SharegptStreamDatasetConverter(DatasetConverter):
+    def __call__(self, example: dict[str, Any]) -> dict[str, Any]:
+        # import pdb; pdb.set_trace()
+        # print("Debug: Converts sharegpt format dataset to the standard format.")
+
+        tag_mapping = {
+            self.dataset_attr.user_tag: Role.USER.value,
+            self.dataset_attr.assistant_tag: Role.ASSISTANT.value,
+            self.dataset_attr.observation_tag: Role.OBSERVATION.value,
+            self.dataset_attr.function_tag: Role.FUNCTION.value,
+            self.dataset_attr.system_tag: Role.SYSTEM.value,
+        }
+        odd_tags = (self.dataset_attr.user_tag, self.dataset_attr.observation_tag)
+        even_tags = (self.dataset_attr.assistant_tag, self.dataset_attr.function_tag)
+        accept_tags = (odd_tags, even_tags)
+        messages = example[self.dataset_attr.messages]
+        if (
+            self.dataset_attr.system_tag
+            and len(messages) != 0
+            and messages[0][self.dataset_attr.role_tag] == self.dataset_attr.system_tag
+        ):
+            system = messages[0][self.dataset_attr.content_tag]
+            messages = messages[1:]
+        else:
+            system = example[self.dataset_attr.system] if self.dataset_attr.system else ""
+
+        # # stream 格式不严格按照user，assistant交错的方式组织，所以跳过验证
+        aligned_messages = []
+        broken_data = False
+
+        # # stream 格式不严格按照user，assistant交错的方式组织，所以跳过验证
+        # for turn_idx, message in enumerate(messages):
+        #     if message[self.dataset_attr.role_tag] not in accept_tags[turn_idx % 2]:
+        #         logger.warning_rank0(f"Invalid role tag in {messages}.")
+        #         broken_data = True
+        #         break
+        #
+        #     aligned_messages.append(
+        #         {
+        #             "role": tag_mapping[message[self.dataset_attr.role_tag]],
+        #             "content": message[self.dataset_attr.content_tag],
+        #         }
+        #     )
+        #
+        # if (not self.dataset_attr.ranking and len(aligned_messages) % 2 != 0) or (
+        #     self.dataset_attr.ranking and len(aligned_messages) % 2 == 0
+        # ):
+        #     logger.warning_rank0(f"Invalid message count in {messages}.")
+        #     broken_data = True
+
+        '''stream 数据存在1个问题多轮回答的形式，不严格按照user，assistant交错的方式组织，所以跳过验证'''
+        for turn_idx, message in enumerate(messages):
+            '''现在已经不再使用messages中的time了'''
+            # # time 统一用float, 防止不匹配
+            # time = message.get('time', None)
+            # if time is not None:
+            #     time = [float(t) for t in time]
+
+            aligned_messages.append({
+                "role": tag_mapping[message[self.dataset_attr.role_tag]],
+                "content": message[self.dataset_attr.content_tag],
+                # "time": time,
+                "valid": message.get('valid', True),  # valid 默认为True
+                "ignore_end_stream": message.get("ignore_end_stream", False),  # ignore_end_stream 默认为False
+                })
+
+        if broken_data:
+            logger.warning_rank0("Skipping this abnormal example.")
+            prompt, response = [], []
+        elif self.dataset_attr.kto_tag and isinstance(example[self.dataset_attr.kto_tag], bool):  # kto example
+            prompt = aligned_messages[:-1]
+            response = aligned_messages[-1:]
+            if example[self.dataset_attr.kto_tag]:
+                response = response + [{"role": Role.ASSISTANT.value, "content": ""}]
+            else:
+                response = [{"role": Role.ASSISTANT.value, "content": ""}] + response
+        elif (
+            self.dataset_attr.ranking
+            and isinstance(example[self.dataset_attr.chosen], dict)
+            and isinstance(example[self.dataset_attr.rejected], dict)
+        ):  # pairwise example
+            chosen = example[self.dataset_attr.chosen]
+            rejected = example[self.dataset_attr.rejected]
+            if (
+                chosen[self.dataset_attr.role_tag] not in accept_tags[-1]
+                or rejected[self.dataset_attr.role_tag] not in accept_tags[-1]
+            ):
+                logger.warning_rank0(f"Invalid role tag in {[chosen, rejected]}.")
+                broken_data = True
+
+            prompt = aligned_messages
+            response = [
+                {
+                    "role": tag_mapping[chosen[self.dataset_attr.role_tag]],
+                    "content": chosen[self.dataset_attr.content_tag],
+                },
+                {
+                    "role": tag_mapping[rejected[self.dataset_attr.role_tag]],
+                    "content": rejected[self.dataset_attr.content_tag],
+                },
+            ]
+        else:  # normal example
+            prompt = aligned_messages[:-1]
+            response = aligned_messages[-1:]
+
+        output = {
+            "_prompt": prompt,
+            "_response": response,
+            "_system": system,
+            "_tools": example[self.dataset_attr.tools] if self.dataset_attr.tools else "",
+            "_images": self._find_medias(example[self.dataset_attr.images]) if self.dataset_attr.images else None,
+            "_videos": self._find_medias(example[self.dataset_attr.videos]) if self.dataset_attr.videos else None,
+            "_audios": self._find_medias(example[self.dataset_attr.audios]) if self.dataset_attr.audios else None,
+        }
+        return output
+
+
+
 DATASET_CONVERTERS = {
     "alpaca": AlpacaDatasetConverter,
     "sharegpt": SharegptDatasetConverter,
+    "sharegpt_stream": SharegptStreamDatasetConverter
 }
 
 
